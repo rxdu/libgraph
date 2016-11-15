1ac52682affc1eee7b41c44509090a72347deb43
rdu <ruixiang.du@gmail.com>
2016-11-15 12:15:14 -0500

    saved work

--- src/graph/astar.h
+++ src/graph/astar.h
@@ -59,221 +59,242 @@ class AStar{
 
 public:
 
-template<typename GraphBDSType, typename GraphVertexType>
-static std::vector<GraphVertexType*> Search(Graph<GraphBDSType>& graph, GraphVertexType *start, GraphVertexType *goal)
-{
-	// reset last search information
-	graph.ResetGraphVertices();
-
-	// start a new search and return result
-	return Search(start, goal);
-}
-
-template<typename GraphBDSType, typename GraphVertexType>
-static std::vector<GraphVertexType*> Search(std::shared_ptr<Graph<GraphBDSType>> graph, GraphVertexType *start, GraphVertexType *goal)
-{
-	// reset last search information
-	graph->ResetGraphVertices();
-
-	// start a new search and return result
-	return Search(start, goal);
-}
-
-template<typename GraphBDSType, typename GraphVertexType>
-static std::vector<GraphVertexType*> Search(Graph<GraphBDSType>* graph, GraphVertexType *start, GraphVertexType *goal)
-{
-	// reset last search information
-	graph->ResetGraphVertices();
-
-	// start a new search and return result
-	return Search(start, goal);
-}
-
-template<typename GraphBDSType>
-static std::vector<Vertex<GraphBDSType>*> IncSearch(GraphBDSType start, GraphBDSType goal, const std::function<std::vector<std::tuple<GraphBDSType, double>>(GraphBDSType)>& get_neighbour_bds)
-{
-	Graph<GraphBDSType> graph;
-
-	bool found_path = false;
-	std::vector<Vertex<GraphBDSType>*> path;
-	Vertex<GraphBDSType>* current_vertex;
-	// open list - a list of vertices that need to be checked out
-	PriorityQueue<Vertex<GraphBDSType>*> openlist;
-
-	// first add start and goal node to graph
-	Vertex<GraphBDSType>* start_vtx = graph.AddVertex(start);
-	Vertex<GraphBDSType>* goal_vtx = graph.AddVertex(goal);
-
-	openlist.put(start_vtx, 0);
-	start_vtx->is_in_openlist_ = true;
-
-	//start->search_parent_ = start;
-	start_vtx->g_astar_ = 0;
-
-	while(!openlist.empty() && found_path != true)
+	/// Search using vertices
+	template<typename GraphBDSType, typename GraphVertexType>
+	static std::vector<GraphVertexType*> Search(Graph<GraphBDSType>& graph, GraphVertexType *start, GraphVertexType *goal)
 	{
-		current_vertex = openlist.get();
-		if(current_vertex->is_checked_)
-			continue;
+		// reset last search information
+		graph.ResetGraphVertices();
 
-		current_vertex->is_in_openlist_ = false;
-		current_vertex->is_checked_ = true;
+		// start a new search and return result
+		return Search(start, goal);
+	}
 
-		std::vector<std::tuple<GraphBDSType, double>> neighbour_bds = get_neighbour_bds(current_vertex->bundled_data_);
-		for(auto& nb : neighbour_bds)
-			graph.AddEdge(current_vertex->bundled_data_, std::get<0>(nb), std::get<1>(nb));
+	template<typename GraphBDSType, typename GraphVertexType>
+	static std::vector<GraphVertexType*> Search(std::shared_ptr<Graph<GraphBDSType>> graph, GraphVertexType *start, GraphVertexType *goal)
+	{
+		// reset last search information
+		graph->ResetGraphVertices();
 
-		std::vector<Edge<Vertex<GraphBDSType>*>> successors = current_vertex->edges_;
+		// start a new search and return result
+		return Search(start, goal);
+	}
 
-		// check all adjacent vertices (successors of current vertex)
-		for(auto& suc:successors)
+	template<typename GraphBDSType, typename GraphVertexType>
+	static std::vector<GraphVertexType*> Search(Graph<GraphBDSType>* graph, GraphVertexType *start, GraphVertexType *goal)
+	{
+		// reset last search information
+		graph->ResetGraphVertices();
+
+		// start a new search and return result
+		return Search(start, goal);
+	}
+
+	/// Search using vertex ids
+	template<typename GraphBDSType>
+	static std::vector<Vertex<GraphBDSType>*> Search(Graph<GraphBDSType>& graph, uint64_t start_id, uint64_t goal_id)
+	{
+		// reset last search information
+		graph.ResetGraphVertices();
+
+		auto start = graph.GetVertexFromID(start_id);
+		auto goal = graph.GetVertexFromID(goal_id);
+
+		std::vector<Vertex<GraphBDSType>*> empty;
+
+		// start a new search and return result
+		if(start != nullptr && goal != nullptr)
+			return Search(start, goal);
+		else
+			return empty;
+	}
+
+	/// Incremental search
+	template<typename GraphBDSType>
+	static std::vector<Vertex<GraphBDSType>*> IncSearch(GraphBDSType start, GraphBDSType goal, std::function<std::vector<std::tuple<GraphBDSType, double>>(GraphBDSType)> get_neighbour_bds)
+	{
+		Graph<GraphBDSType> graph;
+
+		bool found_path = false;
+		std::vector<Vertex<GraphBDSType>*> path;
+		Vertex<GraphBDSType>* current_vertex;
+		// open list - a list of vertices that need to be checked out
+		PriorityQueue<Vertex<GraphBDSType>*> openlist;
+
+		// first add start and goal node to graph
+		Vertex<GraphBDSType>* start_vtx = graph.AddVertex(start);
+		Vertex<GraphBDSType>* goal_vtx = graph.AddVertex(goal);
+
+		openlist.put(start_vtx, 0);
+		start_vtx->is_in_openlist_ = true;
+
+		//start->search_parent_ = start;
+		start_vtx->g_astar_ = 0;
+
+		while(!openlist.empty() && found_path != true)
 		{
-			Vertex<GraphBDSType>* successor;
-			successor = suc.dst_;
+			current_vertex = openlist.get();
+			if(current_vertex->is_checked_)
+				continue;
 
-			// check if the vertex has been checked (in closed list)
-			if(successor->is_checked_ == false)
+			current_vertex->is_in_openlist_ = false;
+			current_vertex->is_checked_ = true;
+
+			std::vector<std::tuple<GraphBDSType, double>> neighbour_bds = get_neighbour_bds(current_vertex->bundled_data_);
+			for(auto& nb : neighbour_bds)
+				graph.AddEdge(current_vertex->bundled_data_, std::get<0>(nb), std::get<1>(nb));
+
+			std::vector<Edge<Vertex<GraphBDSType>*>> successors = current_vertex->edges_;
+
+			// check all adjacent vertices (successors of current vertex)
+			for(auto& suc:successors)
 			{
-				// first set the parent of the adjacent vertex to be the current vertex
-				double new_cost = current_vertex->g_astar_ + suc.cost_;
+				Vertex<GraphBDSType>* successor;
+				successor = suc.dst_;
 
-				// if the vertex is not in open list
-				// or if the vertex is in open list but has a higher cost
-				if(successor->is_in_openlist_ == false || new_cost < successor->g_astar_)
+				// check if the vertex has been checked (in closed list)
+				if(successor->is_checked_ == false)
 				{
-					successor->search_parent_ = current_vertex;
-					successor->g_astar_ = new_cost;
+					// first set the parent of the adjacent vertex to be the current vertex
+					double new_cost = current_vertex->g_astar_ + suc.cost_;
 
-					successor->h_astar_ = successor->CalcHeuristic(goal_vtx);
-					successor->f_astar_ = successor->g_astar_ + successor->h_astar_;
+					// if the vertex is not in open list
+					// or if the vertex is in open list but has a higher cost
+					if(successor->is_in_openlist_ == false || new_cost < successor->g_astar_)
+					{
+						successor->search_parent_ = current_vertex;
+						successor->g_astar_ = new_cost;
 
-					openlist.put(successor, successor->f_astar_);
-					successor->is_in_openlist_ = true;
+						successor->h_astar_ = successor->CalcHeuristic(goal_vtx);
+						successor->f_astar_ = successor->g_astar_ + successor->h_astar_;
 
-					if(successor == goal_vtx){
-						found_path = true;
+						openlist.put(successor, successor->f_astar_);
+						successor->is_in_openlist_ = true;
+
+						if(successor == goal_vtx){
+							found_path = true;
+						}
 					}
 				}
 			}
 		}
-	}
 
-	// reconstruct path from search
-	if(found_path)
-	{
-		std::cout << "path found" << std::endl;
-		Vertex<GraphBDSType>* waypoint = goal_vtx;
-		while(waypoint != start_vtx)
+		// reconstruct path from search
+		if(found_path)
 		{
+			std::cout << "path found" << std::endl;
+			Vertex<GraphBDSType>* waypoint = goal_vtx;
+			while(waypoint != start_vtx)
+			{
+				path.push_back(waypoint);
+				waypoint = waypoint->search_parent_;
+			}
+			// add the start node
 			path.push_back(waypoint);
-			waypoint = waypoint->search_parent_;
-		}
-		// add the start node
-		path.push_back(waypoint);
-		std::reverse(path.begin(), path.end());
+			std::reverse(path.begin(), path.end());
 
-		auto traj_s = path.begin();
-		auto traj_e = path.end() - 1;
+			auto traj_s = path.begin();
+			auto traj_e = path.end() - 1;
 #ifdef MINIMAL_PRINTOUT
-		std::cout << "starting vertex id: " << (*traj_s)->vertex_id_ << std::endl;
-		std::cout << "finishing vertex id: " << (*traj_e)->vertex_id_ << std::endl;
-		std::cout << "path length: " << path.size() << std::endl;
-		std::cout << "total cost: " << path.back()->g_astar_ << std::endl;
+			std::cout << "starting vertex id: " << (*traj_s)->vertex_id_ << std::endl;
+			std::cout << "finishing vertex id: " << (*traj_e)->vertex_id_ << std::endl;
+			std::cout << "path length: " << path.size() << std::endl;
+			std::cout << "total cost: " << path.back()->g_astar_ << std::endl;
 #endif
-	}
-	else
-		std::cout << "failed to find a path" << std::endl;
+		}
+		else
+			std::cout << "failed to find a path" << std::endl;
 
-	return path;
-};
+		return path;
+	};
 
 private:
 
-template<typename GraphVertexType>
-static std::vector<GraphVertexType*> Search(GraphVertexType *start, GraphVertexType *goal)
-{
-	bool found_path = false;
-	std::vector<GraphVertexType*> trajectory;
-	GraphVertexType* current_vertex;
-	// open list - a list of vertices that need to be checked out
-	PriorityQueue<GraphVertexType*> openlist;
-
-	openlist.put(start, 0);
-	start->is_in_openlist_ = true;
-
-	//start->search_parent_ = start;
-	start->g_astar_ = 0;
-
-	while(!openlist.empty() && found_path != true)
+	template<typename GraphVertexType>
+	static std::vector<GraphVertexType*> Search(GraphVertexType *start, GraphVertexType *goal)
 	{
-		current_vertex = openlist.get();
-		if(current_vertex->is_checked_)
-			continue;
+		bool found_path = false;
+		std::vector<GraphVertexType*> trajectory;
+		GraphVertexType* current_vertex;
+		// open list - a list of vertices that need to be checked out
+		PriorityQueue<GraphVertexType*> openlist;
 
-		current_vertex->is_in_openlist_ = false;
-		current_vertex->is_checked_ = true;
+		openlist.put(start, 0);
+		start->is_in_openlist_ = true;
 
-		// check all adjacent vertices (successors of current vertex)
-		for(auto ite = current_vertex->edges_.begin(); ite != current_vertex->edges_.end(); ite++)
+		//start->search_parent_ = start;
+		start->g_astar_ = 0;
+
+		while(!openlist.empty() && found_path != true)
 		{
-			GraphVertexType* successor;
-			successor = (*ite).dst_;
+			current_vertex = openlist.get();
+			if(current_vertex->is_checked_)
+				continue;
 
-			// check if the vertex has been checked (in closed list)
-			if(successor->is_checked_ == false)
+			current_vertex->is_in_openlist_ = false;
+			current_vertex->is_checked_ = true;
+
+			// check all adjacent vertices (successors of current vertex)
+			for(auto ite = current_vertex->edges_.begin(); ite != current_vertex->edges_.end(); ite++)
 			{
-				// first set the parent of the adjacent vertex to be the current vertex
-				double new_cost = current_vertex->g_astar_ + (*ite).cost_;
+				GraphVertexType* successor;
+				successor = (*ite).dst_;
 
-				// if the vertex is not in open list
-				// or if the vertex is in open list but has a higher cost
-				if(successor->is_in_openlist_ == false || new_cost < successor->g_astar_)
+				// check if the vertex has been checked (in closed list)
+				if(successor->is_checked_ == false)
 				{
-					successor->search_parent_ = current_vertex;
-					successor->g_astar_ = new_cost;
+					// first set the parent of the adjacent vertex to be the current vertex
+					double new_cost = current_vertex->g_astar_ + (*ite).cost_;
 
-					successor->h_astar_ = successor->CalcHeuristic(goal);
-					successor->f_astar_ = successor->g_astar_ + successor->h_astar_;
+					// if the vertex is not in open list
+					// or if the vertex is in open list but has a higher cost
+					if(successor->is_in_openlist_ == false || new_cost < successor->g_astar_)
+					{
+						successor->search_parent_ = current_vertex;
+						successor->g_astar_ = new_cost;
 
-					openlist.put(successor, successor->f_astar_);
-					successor->is_in_openlist_ = true;
+						successor->h_astar_ = successor->CalcHeuristic(goal);
+						successor->f_astar_ = successor->g_astar_ + successor->h_astar_;
 
-					if(successor == goal){
-						found_path = true;
+						openlist.put(successor, successor->f_astar_);
+						successor->is_in_openlist_ = true;
+
+						if(successor == goal){
+							found_path = true;
+						}
 					}
 				}
 			}
 		}
-	}
 
-	// reconstruct path from search
-	if(found_path)
-	{
-		std::cout << "path found" << std::endl;
-		GraphVertexType* waypoint = goal;
-		while(waypoint != start)
+		// reconstruct path from search
+		if(found_path)
 		{
+			std::cout << "path found" << std::endl;
+			GraphVertexType* waypoint = goal;
+			while(waypoint != start)
+			{
+				trajectory.push_back(waypoint);
+				waypoint = waypoint->search_parent_;
+			}
+			// add the start node
 			trajectory.push_back(waypoint);
-			waypoint = waypoint->search_parent_;
-		}
-		// add the start node
-		trajectory.push_back(waypoint);
-		std::reverse(trajectory.begin(), trajectory.end());
+			std::reverse(trajectory.begin(), trajectory.end());
 
-		auto traj_s = trajectory.begin();
-		auto traj_e = trajectory.end() - 1;
+			auto traj_s = trajectory.begin();
+			auto traj_e = trajectory.end() - 1;
 #ifdef MINIMAL_PRINTOUT
-		std::cout << "starting vertex id: " << (*traj_s)->vertex_id_ << std::endl;
-		std::cout << "finishing vertex id: " << (*traj_e)->vertex_id_ << std::endl;
-		std::cout << "path length: " << trajectory.size() << std::endl;
-		std::cout << "total cost: " << trajectory.back()->g_astar_ << std::endl;
+			std::cout << "starting vertex id: " << (*traj_s)->vertex_id_ << std::endl;
+			std::cout << "finishing vertex id: " << (*traj_e)->vertex_id_ << std::endl;
+			std::cout << "path length: " << trajectory.size() << std::endl;
+			std::cout << "total cost: " << trajectory.back()->g_astar_ << std::endl;
 #endif
-	}
-	else
-		std::cout << "failed to find a path" << std::endl;
+		}
+		else
+			std::cout << "failed to find a path" << std::endl;
 
-	return trajectory;
-};
+		return trajectory;
+	};
 
 };
 
---
+++ src/tests/performance_test.cpp
@@ -0,0 +1,214 @@
+/*
+ * performance_test.cpp
+ *
+ *  Created on: Nov 15, 2016
+ *      Author: rdu
+ */
+
+// standard libaray
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <cmath>
+#include <cstdint>
+#include <algorithm>
+#include <ctime>
+
+// user
+#include "graph/astar.h"
+
+using namespace srcl_ctrl;
+
+#define GRID_SIZE 100
+
+typedef struct
+{
+	int64_t x;
+	int64_t y;
+} Index;
+
+struct SquareCell: public BDSBase<SquareCell>
+{
+	SquareCell(uint64_t id):
+		BDSBase<SquareCell>(id){};
+	~SquareCell(){};
+
+	Index idx;
+
+	double GetHeuristic(const SquareCell& other_struct) const {
+		return std::abs(idx.x - other_struct.idx.x) + std::abs(idx.y - other_struct.idx.y);
+	}
+};
+
+// index of square grid: positive x points towards right, positive y points upwards
+class GetSquareCellNeighbour
+{
+public:
+	GetSquareCellNeighbour(int grid_size_row, int grid_size_col, double size, const std::vector<uint64_t> obstacle_ids):
+		row_size_(grid_size_row),
+		col_size_(grid_size_col),
+		cell_size_(size),
+		obstacle_ids_(obstacle_ids){};
+
+private:
+	int row_size_;
+	int col_size_;
+	double cell_size_;
+	std::vector<uint64_t> obstacle_ids_;
+
+public:
+	// define the functor operator
+	std::vector<std::tuple<SquareCell, double>> operator()(SquareCell cell)
+	{
+		std::vector<std::tuple<SquareCell, double>> adjacent_cells;
+
+		Index pos[4];
+
+		pos[0].x = cell.idx.x;
+		pos[0].y = cell.idx.y + 1;
+
+		pos[1].x = cell.idx.x;
+		pos[1].y = cell.idx.y - 1;
+
+		pos[2].x = cell.idx.x + 1;
+		pos[2].y = cell.idx.y;
+
+		pos[3].x = cell.idx.x - 1;
+		pos[3].y = cell.idx.y;
+
+		for(int i = 0; i < 4; i++)
+		{
+			if(pos[i].x >= 0 && pos[i].x < col_size_ &&
+					pos[i].y >= 0 && pos[i].y < row_size_)
+			{
+				uint64_t new_id = pos[i].y * col_size_ + pos[i].x;
+
+				if(std::find(obstacle_ids_.begin(), obstacle_ids_.end(), new_id) != obstacle_ids_.end())
+					continue;
+
+				SquareCell cell(new_id);
+				cell.idx.x = pos[i].x;
+				cell.idx.y = pos[i].y;
+
+				adjacent_cells.emplace_back(cell, cell_size_);
+			}
+		}
+
+		return adjacent_cells;
+	}
+};
+
+std::vector<std::tuple<SquareCell, double>> GetNeighbours(SquareCell cell, std::vector<uint64_t>& obstacle_ids)
+{
+	std::vector<std::tuple<SquareCell, double>> adjacent_cells;
+
+	Index pos[4];
+
+	pos[0].x = cell.idx.x;
+	pos[0].y = cell.idx.y + 1;
+
+	pos[1].x = cell.idx.x;
+	pos[1].y = cell.idx.y - 1;
+
+	pos[2].x = cell.idx.x + 1;
+	pos[2].y = cell.idx.y;
+
+	pos[3].x = cell.idx.x - 1;
+	pos[3].y = cell.idx.y;
+
+	for(int i = 0; i < 4; i++)
+	{
+		if(pos[i].x >= 0 && pos[i].x < GRID_SIZE &&
+				pos[i].y >= 0 && pos[i].y < GRID_SIZE)
+		{
+			uint64_t new_id = pos[i].y * GRID_SIZE + pos[i].x;
+
+			if(std::find(obstacle_ids.begin(), obstacle_ids.end(), new_id) != obstacle_ids.end())
+				continue;
+
+			SquareCell cell(new_id);
+			cell.idx.x = pos[i].x;
+			cell.idx.y = pos[i].y;
+
+			adjacent_cells.emplace_back(cell, 1.0);
+		}
+	}
+
+	return adjacent_cells;
+}
+
+int main(int argc, char** argv )
+{
+	clock_t		exec_time;
+	exec_time = clock();
+	SquareCell cell_s(0);
+	cell_s.idx.x = 0;
+	cell_s.idx.y = 0;
+
+	SquareCell cell_g(9595);
+	cell_g.idx.x = 95;
+	cell_g.idx.y = 95;
+
+	std::vector<uint64_t> obstacle_ids;
+	obstacle_ids.push_back(5);
+	obstacle_ids.push_back(6);
+	obstacle_ids.push_back(7);
+	obstacle_ids.push_back(17);
+	obstacle_ids.push_back(18);
+	obstacle_ids.push_back(19);
+	for(int i = 0; i < 50; i++)
+		obstacle_ids.push_back(GRID_SIZE * 10 + i);
+	for(int i = 30; i < 80; i++)
+		obstacle_ids.push_back(GRID_SIZE * 30 + i);
+	for(int i = 20; i < 90; i++)
+		obstacle_ids.push_back(GRID_SIZE * 60 + i);
+//	for(int i = 50; i < GRID_SIZE; i++)
+//		obstacle_ids.push_back(GRID_SIZE * 80 + i);
+
+	auto path = AStar::IncSearch(cell_s, cell_g, GetNeighbourBDSFunc_t<SquareCell>(GetSquareCellNeighbour(GRID_SIZE, GRID_SIZE, 1.0, obstacle_ids)));
+	exec_time = clock() - exec_time;
+	std::cout << "Incremental search finished in " << double(exec_time)/CLOCKS_PER_SEC << " s." << std::endl;
+
+//	for(auto& e : path)
+//		std::cout << "id: " << e->vertex_id_ << std::endl;
+
+	std::cout << "\n----------------------------------------------------------\n" << std::endl;
+
+	exec_time = clock();
+	Graph_t<SquareCell> graph2;
+	std::vector<SquareCell> cells;
+
+	for(int i = 0; i < GRID_SIZE; i++)
+		for(int j = 0; j < GRID_SIZE; j++)
+		{
+			uint64_t new_id = j * GRID_SIZE + i;
+			SquareCell new_cell(new_id);
+			new_cell.idx.x = i;
+			new_cell.idx.y = j;
+			cells.push_back(new_cell);
+		}
+
+	for(auto& c : cells)
+	{
+		std::vector<std::tuple<SquareCell, double>> nbs = GetNeighbours(c, obstacle_ids);
+
+		for(auto& nc : nbs)
+		{
+			graph2.AddEdge(c, std::get<0>(nc), std::get<1>(nc));
+		}
+	}
+
+	auto path2 = AStar::Search(graph2, cells[0].data_id_, cells[9595].data_id_);
+	if(path2.empty())
+		std::cout << "search failed" << std::endl;
+	exec_time = clock() - exec_time;
+	std::cout << "Normal graph search finished in " << double(exec_time)/CLOCKS_PER_SEC << " s." << std::endl;
+
+//	for(auto& e : path2)
+//		std::cout << "id: " << e->vertex_id_ << std::endl;
+
+	return 0;
+}
+
+
+
